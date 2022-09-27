be0e0d831c8991f7b91db76c71d5341d3f9f1ca4
ashr-aful007 <ashraful99980@gmail.com>
2022-09-27 20:58:21 +0600

    change file management

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,17 +3,17 @@ import './Header.css'
 const Header = () => {
      return (
           <nav className='header-section'>
-               <div className='mile-lable'>
-                    <h4>MILE DB</h4>
-               </div>
-               <div className='mile-catagory'>
-                    <a href="/thai">Thai</a>
-                    <a href="/italian">Italian</a>
-                    <a href="/Chines">Chines</a>
-                    <a href="/Blog">Blog</a>
-               </div>
-               
-          </nav>
+          <div className='mile-lable'>
+               <h4>MILE DB</h4>
+          </div>
+          <div className='mile-catagory'>
+               <a href="/thai">Thai</a>
+               <a href="/italian">Italian</a>
+               <a href="/Chines">Chines</a>
+               <a href="/Blog">Blog</a>
+          </div>
+          
+     </nav>
      );
 };
 
