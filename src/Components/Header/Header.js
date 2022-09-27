9502dfa926c1ff413a1c7cd7ad16b43261590aec
ashr-aful007 <ashraful99980@gmail.com>
2022-09-27 10:01:45 +0600

    header section done

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+     return (
+          <nav className='header-section'>
+               <div className='mile-lable'>
+                    <h4>MILE DB</h4>
+               </div>
+               <div className='mile-catagory'>
+                    <a href="/thai">Thai</a>
+                    <a href="/italian">Italian</a>
+                    <a href="/Chines">Chines</a>
+                    <a href="/Blog">Blog</a>
+               </div>
+               
+          </nav>
+     );
+};
+
+export default Header;
